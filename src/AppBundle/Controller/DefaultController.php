75ab862d400eb00c2ee95959de6ec3f6d742be57
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2016-11-06 12:19:34 -0200

    reestruturação das views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,13 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('index.twig');
+    }
+
+    public function homeAction()
+    {
+        return $this->render('system/home.twig');
     }
 }
