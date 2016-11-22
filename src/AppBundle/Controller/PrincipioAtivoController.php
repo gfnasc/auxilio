8452dbcc340df7edcd868e548d34ff9a8d7ee79c
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2016-11-22 13:46:48 -0200

    finalizando CRUD dos principios ativos

--- src/AppBundle/Controller/PrincipioAtivoController.php
+++ src/AppBundle/Controller/PrincipioAtivoController.php
@@ -12,6 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\DateTime;
 class PrincipioAtivoController extends Controller
@@ -39,12 +40,12 @@ class PrincipioAtivoController extends Controller
 
         $data = $request->request->all();
 
-        $em = $this->getDoctrine()->getManager();
-        $principio_ativo = $em->getRepository('AppBundle:PrincipioAtivo')->findAll();
-
         if(!$data){
             return $this->render('system/principio-ativo/cadastrar-principio-ativo.twig');
         } else {
+
+            $em = $this->getDoctrine()->getManager();
+            $principio_ativo = $em->getRepository('AppBundle:PrincipioAtivo')->findAll();
 
             $pa = new PrincipioAtivo();
             $pa->setPrincipioAtivoNome($data['nome']);
@@ -93,6 +94,74 @@ class PrincipioAtivoController extends Controller
             ]);
 
         }
+    }
+
+    /**
+     * @Route("/deletar-principio-ativo/{id}", name="deletar-principio-ativo")
+     */
+    public function deletarPrincipioAtivo($id){
+
+        $em = $this->getDoctrine()->getManager();
+        $pa = $em->getReference('AppBundle:PrincipioAtivo', $id);
+
+        $em->remove($pa);
+        $em->flush();
+
+        $pas = $em->getRepository('AppBundle:PrincipioAtivo')->findAll();
+
+        return $this->render('system/principio-ativo/gerenciar-principio-ativo.twig', [
+            'msg' => 'Paciente excluído com sucesso!',
+            'principio_ativo' => $pas
+        ]);
+    }
+
+    /**
+     * @Route("/buscar-principio-ativo", name="buscar-principio-ativo")
+     * @Method({"GET", "POST"})
+     */
+    public function buscaPaciente(Request $request){
+
+        $term = $request->request->get('term');
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $pas = $qb->select('p')
+            ->from('AppBundle:PrincipioAtivo', 'p')
+            ->where('p.principioAtivoNome LIKE :term')
+            ->setParameter(':term', '%'.$term.'%')
+            ->getQuery()->getResult();
+
+        $html = '
+        <table class="table table-hover">
+                                <tbody id="content-table">
+        ';
+
+        $html .= '
+            <tr>
+                <th>Nome</th>
+                <th></th>
+            </tr>
+        ';
+
+        foreach ($pas as $pa){
+            $html .= '
+                <tr>
+                    <td>'.$pa->getPrincipioAtivoNome().'</td>
+                    <td style="text-align: right;">
+                        <a href="editar-principio-ativo/'.$pa->getCod().'"><button type="button" style="padding: 1px 2px;" class="btn btn-warning btn-flat">Editar</button></a>
+                        <a href="deletar-principio-ativo/'.$pa->getCod().'"><button type="button" style="padding: 1px 2px;" class="btn btn-danger btn-flat">Excluir</button></a>
+                    </td>
+                </tr>
+            ';
+        }
+
+        $html .= '
+        </tbody>
+        </table>
+        ';
+
+
+        return new JsonResponse($html);
     }
 
 }
